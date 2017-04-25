3a0b1cc27dec834b8ec63979a9f5f083f0563f12
tientaitu98 <tientaitu98@gmail.com>
2017-04-26 00:10:16 +0700

    Add files via upload

---
+++ audio.cpp
@@ -0,0 +1,39 @@
+//Using SDL, SDL_image, SDL_ttf, SDL_mixer, standard IO, math, and strings
+#include "audio.h"
+
+void audio(Mix_Music* gTempMusic)
+{
+	//If there is no music playing
+	if( Mix_PlayingMusic() == 0 )
+	{
+		Mix_PlayMusic(gTempMusic,-1);
+	}
+	/*
+	//If music is being played
+	{
+		//If the music is paused
+		if( Mix_PausedMusic() == 1 )
+		{
+			//Resume the music
+			Mix_ResumeMusic();
+		}
+		//If the music is playing
+		else
+		{
+			//Pause the music
+			Mix_PauseMusic();
+		}
+	}
+		case 2:
+		{
+			Mix_HaltMusic();
+			break;
+		}
+		*/	//Stop the music
+
+	//}
+}
+void effect(Mix_Music* gTempMusic)
+{
+	Mix_PlayMusic( gTempMusic, 0 );
+}
