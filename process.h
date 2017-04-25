f94e8688bf1788871525686c4a810d40e735cbd9
tientaitu98 <tientaitu98@gmail.com>
2017-04-26 00:09:27 +0700

    Add files via upload

---
+++ process.h
@@ -0,0 +1,5 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
+void file(short status,short* player_highscore,short* theme);
+void savegame(short starus,short* player_score, short arr_game_screen[11][11], short arr_game_creat[3][4], short* game_block_in);
