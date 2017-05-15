d4755fdb94cedd15ee11bfb93cdea253e10d82c6
tientaitu98 <tientaitu98@gmail.com>
2017-05-15 10:07:33 +0700

    Update audio.cpp

--- audio.cpp
+++ audio.cpp
@@ -1,4 +1,3 @@
-//Using SDL, SDL_image, SDL_ttf, SDL_mixer, standard IO, math, and strings
 #include "audio.h"
 
 void audio(Mix_Music* gTempMusic)
@@ -8,8 +7,8 @@ void audio(Mix_Music* gTempMusic)
 	{
 		Mix_PlayMusic(gTempMusic,-1);
 	}
-	/*
 	//If music is being played
+	else if( Mix_PlayingMusic() == 1 )
 	{
 		//If the music is paused
 		if( Mix_PausedMusic() == 1 )
@@ -24,16 +23,17 @@ void audio(Mix_Music* gTempMusic)
 			Mix_PauseMusic();
 		}
 	}
-		case 2:
-		{
-			Mix_HaltMusic();
-			break;
-		}
-		*/	//Stop the music
+	/*
+	{
+		//Stop the music
+		Mix_HaltMusic();
+	}
 
-	//}
+	*/
+
 }
-void effect(Mix_Music* gTempMusic)
+void effect(Mix_Chunk* gTempMusic)
 {
-	Mix_PlayMusic( gTempMusic, 0 );
+	//Mix_PlayMusic( gTempMusic, 0 );
+	Mix_PlayChannel( -1, gTempMusic, 0 );
 }
