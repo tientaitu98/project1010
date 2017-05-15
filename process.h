c0aa6303b911083f96bbd52698de8c97f055b756
tientaitu98 <tientaitu98@gmail.com>
2017-05-15 10:09:57 +0700

    Update process.h

--- process.h
+++ process.h
@@ -1,5 +1,7 @@
-#include <fstream>
-#include <iostream>
+#include<fstream>
+#include<iostream>
 using namespace std;
-void file(short status,short* player_highscore,short* theme);
+
+void comparision(short* player_score,short* player_highscore,short* challenger_highscore,bool* allow_to_save);
+void file(short status,short* player_highscore, short* challenger_highscore,short* theme);
 void savegame(short starus,short* player_score, short arr_game_screen[11][11], short arr_game_creat[3][4], short* game_block_in);
