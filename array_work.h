884fa19fc5b6e92335d6f153d20bcb09ac22d3a1
tientaitu98 <tientaitu98@gmail.com>
2017-05-15 10:03:57 +0700

    Update array_work.h

--- array_work.h
+++ array_work.h
@@ -6,5 +6,3 @@ void arr_game_creat_random(short arr_game_get_block[18][6],short arr_game_creat[
 void drop(short x,short	y, short shape, short color, short subject,short arr_game_screen[11][11],short* dropin,short* lifesign);
 void cleararea(short area,short arr_game_creat[3][4],short arr_game_get_block[18][6]);
 void check_full_row_or_colum(short arr_game_screen[11][11],short row[11],short colum[11],short* player_score);
-
-
